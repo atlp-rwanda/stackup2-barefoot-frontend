d181a0c69f8aa1fa59ad954662cc65554cb97e2b
Divine Ugizwenayo <ugizwenayodiny@gmail.com>
2020-05-13 10:38:21 -1000

    (chore): setupReduxReactRouter
    - setup redux for managing state
    - setup react router dom for routing in our app
    
    [Finishes #171380683]

---
+++ src/views/frontPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FrontPage = () => (
+  <div>
+    <h1>
+      Welcome to React app
+    </h1>
+    <p>
+      react router dom is well configured!!!
+    </p>
+
+  </div>
+);
+
+export default FrontPage;
