42217f4d3cf2f431219e9dd9487994dfd0e42765
muchristian <mucyochristian2@gmail.com>
2020-06-01 03:30:23 +0200

    feat(Users should be presented with a reset password link)
    Document code using JSDOC
    Under activity, endeavor to come up with the thought process (data flow) needed to implement this feature

---
+++ src/entry/ResetPassword.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class ResetPassword extends Component {
+
+    render() {
+        return (
+            <Fragment>
+                <p>reset password form should go here</p>
+            </Fragment>
+        )
+    }
+
+}
+export default ResetPassword
